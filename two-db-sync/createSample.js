d98392439508e0b42e30773711a0c984ed3aa942
Divyansh Sharma <divyanshsharma4507@gmail.com>
2025-11-08 11:41:35 +0530

    Added createSampleFile

---
+++ two-db-sync/createSample.js
@@ -0,0 +1,60 @@
+//The purpose of this file is to create sample schema and seed data to MASTER database
+
+import dotenv from 'dotenv';
+import pkg from 'pg';
+
+dotenv.config();
+
+const {Client} = pkg;
+
+async function main(){
+
+    const client = new Client({
+        connectionString: process.env.DATABASE_URL_MASTER,
+    })
+
+    try{
+        await client.connect();
+        console.log('Connected to MASTER database');
+
+        await client.query(`
+            CREATE TABLE IF NOT EXISTS users(
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(100) NOT NULL,
+            email TEXT UNIQUE NOT NULL,
+            created_at TIMESTAMP DEFAULT NOW()
+            )`)
+
+        await client.query(`
+            CREATE TABLE IF NOT EXISTS posts(
+            id SERIAL PRIMARY KEY,
+            user_id INTEGER REFERENCES users(id) on DELETE CASCADE,
+            text TEXT NOT NULL,
+            body TEXT,
+            created_at TIMESTAMP DEFAULT NOW() 
+            `)
+
+        await client.query(`
+            INSERT INTO users (name, email)
+            VALUES
+            ('Divyansh', 'connectwithdivyansharma@gmail.com'),
+            ('User', 'user@example.com')
+            ON CONFLICT DO NOTHING;
+            `);
+
+       await client.query(`
+            INSERT INTO posts (user_id, title, body)
+            VALUES
+            ( (SELECT id FROM users WHERE email='connectwithdivyansharma@gmail.com'), 'Hello', 'First post by Divyansh' ),
+            ( (SELECT id FROM users WHERE email='user@gmail.com'), 'Hi', 'First post by User' )
+            ON CONFLICT DO NOTHING;
+            `);
+
+     console.log('Sample schema and data created in MASTER database');
+
+
+    }catch(err){
+        console.error('Error executing query', err.stack);
+    }
+
+}
